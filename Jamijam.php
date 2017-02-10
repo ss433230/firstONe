405dec9faddd5a59a26fa22695f2b6d8bd1e13b7
ss433230 <ss433230@gmail.com>
2017-02-10 13:50:19 -0500

    ss

---
+++ Jamijam.php
@@ -0,0 +1,26 @@
+<?php 
+define('BOT_TOKEN', 'jamchahrbot:373015705:AAFtuEh7VylLWbDZi6_9fLsgxR6bkEAWETs');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+        
+// read incoming info and grab the chatID
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+$chatID = $update["message"]["chat"]["id"];
+                
+// compose reply
+$reply =  sendMessage();
+                
+// send reply
+$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+file_get_contents($sendto);
+
+function sendMessage(){
+$message = "I am a baby bot.";
+return $message;
+}
+ 
+ 
+ 
+ 
+ 
+?>
