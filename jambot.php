c675a8e8feb86cbce47b9278b59659f99b3b48a1
ss433230 <ss433230@gmail.com>
2017-02-10 11:01:18 -0500

    salamo

--- jambot.php
+++ jambot.php
@@ -2,13 +2,31 @@
 $bot Token = "373015705:AAFtuEh7VylLWbDZi6_9fLsgxR6bkEAWETs" ;
 &website = "https://api.telegram.org/bot".$botToken;
 
-$update = file_get_contents(webstie."/getupdates");
+$update = file_get_contents("php://input");
 
 $updateArray = json_decode($update, TRUE);
 
-print_r($updateArray);
+//rint_r($updateArray);
 
-$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
-print_r($chatId);
+//$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
 
+switch ($message) {
+
+case "/test":
+sendmessage($chatId,"test");
+break;
+case "/ht":
+sendMessage($chatId,"hey there!")
+break;
+default:
+sendMessage($chatId,"default");
+}
+//file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
+function sendMessage ($chatId, $message){
+$url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId"&text=".urlencode($message);
+file_get_contents($url);
+
+}
 ?>
