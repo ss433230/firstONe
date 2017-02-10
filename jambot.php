366ffe00ca833d9e9da99670c0e98d675ed9f82e
ss433230 <ss433230@gmail.com>
2017-02-10 13:08:45 -0500

    s

--- jambot.php
+++ jambot.php
@@ -1,34 +1,38 @@
 <?php 
 
-ini_set('error_reporting',E_ALL);
-
-$bot Token = "373015705:AAFtuEh7VylLWbDZi6_9fLsgxR6bkEAWETs" ;
-&website = "https://api.telegram.org/bot".$botToken;
-
-$update = file_get_contents("php://input");
+$botToken = "373015705:AAFtuEh7VylLWbDZi6_9fLsgxR6bkEAWETs" ;
+$website = "https://api.telegram.org/bot".$botToken;
+ 
+$update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
-
-//rint_r($updateArray);
-
-//$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+ 
+ 
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
-
-switch ($message) {
-
-     case "/test":
-              sendmessage($chatId,"test");
-              break;
-     case "/hi":
-              sendMessage($chatId,"hey there!")
-              break;
-     default:
-              sendMessage($chatId,"default");
+ 
+ 
+switch($message) {
+       
+        case "/test":
+                sendMessage($chatId, "test");
+                break;
+        case "/hi":
+                sendMessage($chatId, "hi there!");
+                break;
+        default:
+                sendMessage($chatId, "default");
+       
 }
-//file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
-function sendMessage ($chatId, $message){
-$url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId"&text=".urlencode($message);
-file_get_contents($url);
-
+ 
+function sendMessage ($chatId, $message) {
+       
+        $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
+        file_get_contents($url);
+       
 }
+ 
+ 
+ 
+ 
+ 
 ?>
